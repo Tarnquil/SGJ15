1989746318bc260305d5923472ef6ff8e47537c1
Tarnquil <Tarnquil@gmail.com>
2015-01-25 14:03:25 +0000

    hjk

---
+++ Assets/Script/InstructionScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionScreen : MonoBehaviour
+{
+		GameObject page1;
+		GameObject page2;
+
+		void OpenPage1 ()
+		{
+				page2.SetActive (false);
+				page1.SetActive (true);
+		}
+
+		void OpenPage2 ()
+		{
+				page2.SetActive (true);
+				page1.SetActive (false);
+		}
+
+		void LoadMainMenu ()
+		{
+				Application.LoadLevel ("Menu");
+		}
+}
