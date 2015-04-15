f62a88f2c25e4146f3eb575d1b4e2b31cdc97077
niknar266 <krm266@gmail.com>
2015-04-15 17:58:06 +0100

    Dual Pause Menu

--- Assets/Scripts/DualPauseMenu.cs
+++ Assets/Scripts/DualPauseMenu.cs
@@ -3,16 +3,72 @@ using System.Collections;
 
 public class DualPauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenu1, pauseMenu2;
+    bool quit1 = false,
+        quit2 = false,
+        play1 = false, 
+        play2 = false;
+	
+    GameController controller;
 
-    // Use this for initialization
     void Start()
     {
-	
+        controller = GetComponent<GameController>();
     }
-	
     // Update is called once per frame
     void Update()
     {
-	
+        if (play1 && play2)
+        {
+            PauseButton(false);
+        }
+
+        if (quit1 && quit2)
+        {
+            Application.LoadLevel("Menu");
+        }
+    }
+
+    public void Quit1(bool state)
+    {
+        quit1 = state;
+    }
+
+    public void Quit2(bool state)
+    {
+        quit2 = state;
+    }
+
+    public void Play1(bool state)
+    {
+        play1 = state;
+    }
+
+    public void Play2(bool state)
+    {
+        play2 = state;
+    }
+
+    public void PauseButton(bool state)
+    {
+        if (!state)
+        {
+            quit1 = false;
+            quit2 = false;
+            play1 = false;
+            play2 = false;
+
+            pauseMenu1.SetActive(false);
+            pauseMenu2.SetActive(false);
+
+            controller.currentState = GameController.GameState.BETWEEN_ROUND;
+        }
+        else
+        {
+            pauseMenu1.SetActive(true);
+            pauseMenu2.SetActive(true);
+
+            controller.currentState = GameController.GameState.PAUSED;
+        }
     }
 }
