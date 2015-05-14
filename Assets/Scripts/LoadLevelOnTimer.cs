2e209489f9ee50ebd122fa6fccc82be292b402f3
niknar266 <krm266@gmail.com>
2015-05-14 23:47:27 +0100

    Release ver. Splash screen added.

---
+++ Assets/Scripts/LoadLevelOnTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnTimer : MonoBehaviour
+{
+	public string levelName = "Menu";
+	public float time = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("LoadLevel", time);
+
+	}
+
+	void LoadLevel ()
+	{
+		Application.LoadLevel (levelName);
+	}
+
+}
